c96335c29060d5b6a9d1fe227dfaa6664fa91ef4
TamNhien <canhthien91@gmail.com>
2024-03-28 20:44:15 +0700

    Add project files.

---
+++ P_DoiViTriMax_Min_InList.py
@@ -0,0 +1,5 @@
+﻿L = list(map(int, input("Nhập vào danh sách các số nguyên, cách nhau bởi dấu phẩy: ").split(',')))
+min_index, max_index = L.index(min(L)), L.index(max(L))
+L[min_index], L[max_index] = L[max_index], L[min_index]
+print("Danh sách sau khi biến đổi: ", L)
+
